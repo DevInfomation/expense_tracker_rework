0e4f64b8f8344fe0b89a46556e5579ca39e37e01
stefan-milkovski <stefan.milkovski@generadevelopment.com>
2023-10-16 19:53:28 +0200

    adding all the edited files

--- js/expense.js
+++ js/expense.js
@@ -1,13 +1,23 @@
 const cards = document.getElementById('cards');
-const name = document.getElementById('name');
+const inputName = document.getElementById('name');
 const date = document.getElementById('date');
 const amount = document.getElementById('amount');
 const btn = document.getElementById('main-button');
+const mainTable = document.getElementById('main-table');
 
 btn.addEventListener('click', e => {
     e.preventDefault();
 
-    
+    const typeTD = document.createElement('td');
+    const inputNameTD = document.createElement('td');
+    const dateTD = document.createElement('td');
+    const amountTD = document.createElement('td');
+
+    typeTD.textContent = cards.value;
+    inputNameTD.textContent = inputName.value;
+    date.textContent = date.value;
+    amount.textContent = '$' + amount.value;
+
 
 
 });
