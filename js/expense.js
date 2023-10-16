4962bacb974b23b460a52fb93cb97a20ae650d13
stefan-milkovski <stefan.milkovski@generadevelopment.com>
2023-10-16 20:09:26 +0200

    completed the new expense tracker in a day

--- js/expense.js
+++ js/expense.js
@@ -1,16 +1,16 @@
 const cards = document.getElementById('cards');
 const inputName = document.getElementById('name');
 const date = document.getElementById('date');
-const amount = document.getElementById('amount');
+const amount = document.getElementById('money');
 const btn = document.getElementById('main-button');
-const mainTable = document.getElementById('main-table');
+const tbodyMain = document.getElementById('main-tbody');
 
 btn.addEventListener('click', e => {
     e.preventDefault();
 
     let createTr = document.createElement('tr');
-    createTr.classList.add('table-row-header');
-    mainTable.append(createTr);
+    createTr.classList.add('insert-here');
+    tbodyMain.append(createTr);
 
     const typeTD = document.createElement('td');
     const inputNameTD = document.createElement('td');
@@ -22,6 +22,9 @@ btn.addEventListener('click', e => {
     dateTD.textContent = date.value;
     amountTD.textContent = '$' + amount.value;
 
-
+    createTr.append(typeTD);
+    createTr.append(inputNameTD);
+    createTr.append(dateTD);
+    createTr.append(amountTD);
 
 });
