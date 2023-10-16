28cbb38facb997a6f20045095813cfdeee7b3bb1
stefan-milkovski <stefan.milkovski@generadevelopment.com>
2023-10-16 15:18:25 +0200

    Setting up the button in javascript

---
+++ js/expense.js
@@ -0,0 +1,13 @@
+const cards = document.getElementById('cards');
+const name = document.getElementById('name');
+const date = document.getElementById('date');
+const amount = document.getElementById('amount');
+const btn = document.getElementById('main-button');
+
+btn.addEventListener('click', e => {
+    e.preventDefault();
+
+    
+
+
+});
